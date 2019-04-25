4141cbcd3ce2f2d4404ade060ccc571dfddc835d
Perri Demopulos <perridemop@gmail.com>
2019-04-25 12:55:15 -0500

    Classes created

---
+++ Uno_part_2/Uno_part_2/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno_part_2
+{
+    public class Card : ICloneable
+    {
+        public readonly Rank rank;
+        public readonly Color color;
+
+        public static bool useTrumps = flase;
+
+        public static Color trump = ConsoleC
+    }
+}
