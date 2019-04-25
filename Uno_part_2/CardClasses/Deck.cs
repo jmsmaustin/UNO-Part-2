e3497cc32729ef61940a9f92106737c199784d61
Perri Demopulos <perridemop@gmail.com>
2019-04-25 17:20:20 -0500

    ss

---
+++ Uno_part_2/CardClasses/Deck.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno_part_2
+{
+    public class Deck
+    {
+        private Cards cards = new Cards();
+
+        public Deck()
+        {
+            for (int colorVal = 0; colorVal < 4; colorVal++)
+            {
+                for (int rankVal = 1; rankVal < 14; rankVal++)
+                {
+                    cards.Add(new Cards((Color)colorVal, (Rank)rankVal));
+                }
+            }
+        }
+        public void Shuffle()
+        {
+            Random r = new Random();
+            List<Cards> cardshuffle = new List<Cards>();
+            for (int i = Cards.Count-1; i > 0; --i)
+            {
+                int n = r.Next(i + 1);
+                Card temp = cardshuffle[i];
+                cardshuffle[i] = cardshuffle[n];
+                cardshuffle[n] = temp;
+            }
+            
+        }
+        public List<Cards> Draw (int count)
+        {
+            var drawnCards = Cards.Take(count).ToList();
+            Cards.RemoveAll(x => drawnCards.Contains(x));
+            return drawnCards;
+        }
+    }
+}
---
+++ Uno_part_2/CardClasses/Rank.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno_part_2
+{
+    public enum Rank
+    {
+        One = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Reverse,
+        Skip,
+        DrawTwo,
+    }
+}
