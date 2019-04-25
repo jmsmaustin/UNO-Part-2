8e1bf852761da9cc6687c9421b34e9d0bb623d34
Perri Demopulos <perridemop@gmail.com>
2019-04-25 17:16:41 -0500

    classes redone

---
+++ Uno_part_2/CardClasses/CardOutOfRangeException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardClasses
+{
+    public class CardOutOfRangeException : Exception
+    {
+        private Cards deckContents;
+
+        public Cards DeckContents
+        {
+            get { return deckContents; }
+        }
+
+        public CardOutOfRangeException(Cards sourceDeskContents)
+            : base("There are only 52 cards in the deck.")
+        {
+            deckContents = sourceDeskContents;
+        }
+
+    }
+}
